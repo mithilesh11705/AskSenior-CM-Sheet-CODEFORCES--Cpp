c579dff3ce2472face3a989ad6fb38f1124e541a
mithilesh11705 <mithileshdeore7@gmail.com>
2025-01-16 19:31:10 +0530

    Update Basic_DataTypes.cpp

--- Cpp-Basics/Basic_DataTypes.cpp
+++ Cpp-Basics/Basic_DataTypes.cpp
@@ -3,16 +3,13 @@ using namespace std;
 
 int main()
 {
-
     int k;
     char c;
     double d;
     long long l;
     float f;
-
-
+    
     cin>>k>>l>>c>>f>>d;
-
     cout<<k<<"\n"<<l<<"\n"<<c<<"\n"<<f<<"\n"<<d;
 
     return 0;
