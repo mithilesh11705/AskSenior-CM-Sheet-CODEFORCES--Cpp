461fc158da61a0979ec21808cacb13cdb934e653
mithilesh11705 <mithileshdeore7@gmail.com>
2025-01-16 19:29:04 +0530

    Add files via upload

---
+++ Cpp-Basics/Basic_DataTypes.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int k;
+    char c;
+    double d;
+    long long l;
+    float f;
+
+
+    cin>>k>>l>>c>>f>>d;
+
+    cout<<k<<"\n"<<l<<"\n"<<c<<"\n"<<f<<"\n"<<d;
+
+    return 0;
+}
