4cef41ffdf6dad5978ce0606d4d9cc4e5c1687a9
mithilesh11705 <mithileshdeore7@gmail.com>
2025-01-19 11:12:26 +0530

    Area of Circle.cpp

---
+++ Cpp-Basics/Area of Circle.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	double Pi=3.141592653;
+	double r;
+	cin>>r;
+
+double a=Pi*r*r;
+cout<<fixed<<setprecision(9);
+cout<<a<<endl;
+	return 0;
+}
